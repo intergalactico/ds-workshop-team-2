f81201e3dfe401e190f55d94ca3e923f99606d14
Daniel Schildt <daniel.schildt@gmail.com>
2018-03-15 14:31:55 +0200

    fix(Footer): Improve footer styles
    - Improve footer styles
    - Use global colorset for the colours

--- src/Footer/styles.js
+++ src/Footer/styles.js
@@ -1,16 +1,17 @@
 import styled, { css } from "styled-components";
 
 import * as ts from "../Typography/styles.js";
+import colorset from '../Colors/colorset.js';
 
 export const Footer = styled.div`
-  background-color: black;
-  color: white;
+  background-color: ${colorset.backgroundgrey};
+  color: ${colorset.white};
   text-align: center;
 `;
 
 // We're extending <ts.P> component with extra styles
 export const P = ts.P.extend`
-  color: white;
+  color: ${colorset.white};
   padding: 15px 0 15px 0;
   font-size: 14px;
 `;
